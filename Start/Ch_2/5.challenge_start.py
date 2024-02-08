88c68d75affca684c6b404afb6cf5a0603ef636a
myfriendarnab-habijabi <81508191+myfriendarnab-habijabi@users.noreply.github.com>
2024-02-08 13:37:29 +0000

    ch2 challenge completed and removed vscode from gitignore

--- Start/Ch_2/5.challenge_start.py
+++ Start/Ch_2/5.challenge_start.py
@@ -1,9 +1,15 @@
 # Example file for Advanced Python: Working With Data by Joe Marini
 # Programming challenge: use advanced data collections on the earthquake data
+# Show all events by their type and count
 
 import json
-
+from collections import Counter 
 
 # open the data file and load the JSON
 with open("../../30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+quakes = list(map(lambda q:q["properties"]["type"], data["features"]))
+
+for type,count in Counter(quakes).items():
+    print(f'{type}:{count}')
